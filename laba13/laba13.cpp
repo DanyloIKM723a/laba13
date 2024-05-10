d48d6c2ced544ecaef60d5b0d4171e9bd104bd58
grush <grush@DESKTOP-P5A2MM5>
2024-05-10 09:52:22 +0300

    Добавьте файлы проекта.

---
+++ laba13/laba13.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int computeSeries(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return computeSeries(n - 1) * 4;
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+    std::cout << "The " << n << "-th term of the series is: " << computeSeries(n) << std::endl;
+    return 0;
+}
